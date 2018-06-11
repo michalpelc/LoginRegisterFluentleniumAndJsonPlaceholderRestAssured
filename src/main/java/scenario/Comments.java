c7b42156e0eac65ed1d563c5462195eb5ee48a0d
Michal Pelc <michal.pelc@first-utility.com>
2018-06-11 18:16:57 +0200

    Finished RestAssured tests.

---
+++ src/main/java/scenario/Comments.java
@@ -0,0 +1,11 @@
+package scenario;
+
+class Comments {
+
+    private String body;
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+}
--- src/main/java/scenario/JsonPlaceholderTest.java
+++ src/main/java/scenario/JsonPlaceholderTest.java
@@ -1,14 +1,17 @@
 package scenario;
 
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.response.Response;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.List;
 
-import static com.jayway.restassured.RestAssured.baseURI;
-import static com.jayway.restassured.RestAssured.when;
+import static com.jayway.restassured.RestAssured.*;
 import static java.lang.System.out;
 import static java.util.Collections.reverseOrder;
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class JsonPlaceholderTest {
@@ -18,10 +21,14 @@ public class JsonPlaceholderTest {
         baseURI = "https://jsonplaceholder.typicode.com";
     }
 
+    private static Response response;
+    private final Comments comments = new Comments();
+
     @Test
     public void shouldPrintHighestUserIdValue() {
         List<Integer> userIds = getUserIdList();
         userIds.sort(reverseOrder());
+
         Integer highestUserIdValue = userIds.get(0);
         out.println("Highest userId value from response is: " + highestUserIdValue);
     }
@@ -30,26 +37,43 @@ public class JsonPlaceholderTest {
     public void shouldPrintHighestIdValueForUserId() {
         List<Integer> userIds = getUserIdList();
         Integer userId = userIds.get(0);
+
         List<Integer> idsForUserId = getIdListForUserId(userId);
         idsForUserId.sort(reverseOrder());
+
         Integer highestIdValueForUserId = idsForUserId.get(0);
         out.println("Highest id value for userId " + userId + " is: " + highestIdValueForUserId);
     }
 
     @Test
     public void shouldAddNewCommentForPostId() {
+        Integer identifier = getUserIdList().get(0);
+        String testValue = "test_comment";
+
+        comments.setBody(testValue);
+
+        response =
+                given().body(comments)
+                        .when().contentType(ContentType.JSON)
+                        .put("/comments/" + identifier)
+                        .then().statusCode(SC_OK)
+                        .extract().response();
+
+        assertThat(response.asString()).contains(testValue);
+
     }
+
 
     private List<Integer> getIdListForUserId(Integer userId) {
         return when().get("/posts?userId=" + userId)
-                .then().statusCode(200)
+                .then().statusCode(SC_OK)
                 .extract().response()
                 .jsonPath().get("id");
     }
 
     private List<Integer> getUserIdList() {
         return when().get("/posts")
-                .then().statusCode(200)
+                .then().statusCode(SC_OK)
                 .extract().response()
                 .jsonPath().get("userId");
     }
